9ba90cefebf0540eb24abbb8242333263ab64cb8
ebuenting5 <buenting5@tritons.iowacentral.edu>
2022-09-28 10:23:58 -0500

    v2

--- Lab 3 User and File IO/main.cpp
+++ Lab 3 User and File IO/main.cpp
@@ -2,21 +2,23 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
+//stating that namespace will remain in STD
 using namespace std;
 float userinput();
 float means();
 float standarddeviationuser();
+//First three functions are used for user calculated population
 float fileinput();
-float fileoutput();
-float standarddeviation();
 float mean2();
+float standarddeviation();
+float fileoutput();
+//These 4 functions are for the file IO population
 int i, j;
 float sum = 0.0, average, SDT = 0.0, SD, num[4], sum2 = 0.0, average2, SDT2 = 0.0, SD2, Num[4];
-
+//All variable used in this project were created as global in order to ensure transferability between fuctions
 string inputfile = "inMeanStd.dat";
 string outputfile = "outMeanStd.dat";
-
+//Strings were set up for the files in order to help reduce 
 int main() {
 	userinput();
 	means();
@@ -29,6 +31,7 @@ int main() {
 	fileoutput();
 	return 0;
 }
+//All calculations were done within functions and called in main, with visual outputs being declared here
 float userinput() {
 	for (i = 0; i < 4; ++i)
 	{
@@ -38,14 +41,17 @@ float userinput() {
 	}
 	return 0;
 }
+//This uses a loop and an array to store any 4 user input number
 float means() {
 	average = sum / 4;
 	return 0;
 }
+//The mean of the user data
 float mean2() {
 	average2 = sum2 / 4;
 	return 0;
 }
+//The mean of the file data
 float standarddeviationuser() {
 	for (i = 0; i < 4; ++i) {
 		SDT += pow(num[i] - average, 2);
@@ -53,6 +59,7 @@ float standarddeviationuser() {
 	SD = pow(SDT / 4, 0.5);
 	return 0;
 }
+//The standard deviation of the user inputted data
 float fileinput() {
 	ifstream infile;
 	infile.open(inputfile);
@@ -64,6 +71,7 @@ float fileinput() {
 	}
 	return 0;
 }
+//The file is opened in this function and its values are stored into a seperate array
 float fileoutput() {
 	ofstream outfile;
 	outfile.open(outputfile);
@@ -71,6 +79,7 @@ float fileoutput() {
 	outfile << "The mean is: " << average2 << endl << "The standard deviation is: " << SD2;
 	return 0;
 }
+//The output file is opened and the desired data is displayed inside
 float standarddeviation() {
 	for (j = 0; j < 4; ++j) {
 		SDT2 += pow(Num[j] - average2, 2);
@@ -78,3 +87,4 @@ float standarddeviation() {
 	SD2 = pow(SDT2 / 4, 0.5);
 	return 0;
 }
+//The standard devaition of the file data is calculated here
